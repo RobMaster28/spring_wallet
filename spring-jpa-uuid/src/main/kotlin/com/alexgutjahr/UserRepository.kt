c21e9908a62b0293a1041a93d440dfe16a8be741
SergioAntonio <111243440+sergioantonioar@users.noreply.github.com>
2025-06-30 19:23:31 -0500

    Delete spring-jpa-uuid directory

--- spring-jpa-uuid/src/main/kotlin/com/alexgutjahr/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.alexgutjahr
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-import java.util.UUID
-
-@Repository
-interface UserRepository : CrudRepository<UserEntity, UUID>
