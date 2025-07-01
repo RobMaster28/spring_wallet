6b9daef670b3ee452097032ff01f361060cc2816
SergioAntonio <111243440+sergioantonioar@users.noreply.github.com>
2025-06-30 19:23:01 -0500

    Delete spring-crud-api directory

--- spring-crud-api/src/main/kotlin/com/alexgutjahr/UserRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.alexgutjahr
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository : CrudRepository<User, String>
