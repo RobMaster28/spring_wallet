75ae1a8fe3caa6b66b4a2082c58ea69d8b9a1744
SergioAntonio <111243440+sergioantonioar@users.noreply.github.com>
2025-06-30 19:22:47 -0500

    Delete spring-aop directory

--- spring-aop/src/main/kotlin/com/alexgutjahr/TwitterProperties.kt
+++
@@ -1,8 +0,0 @@
-package com.alexgutjahr
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
-
-@ConstructorBinding
-@ConfigurationProperties(prefix = "twitter")
-data class TwitterProperties(val bearer: String)
